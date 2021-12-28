44bb46215be668ca2223f3250e97aee2a6bdb8d9
STRRL <str_ruiling@outlook.com>
2021-12-28 23:23:37 +0800

    chore: update gradle configuration
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,5 @@ pluginManagement {
     plugins {
         val loomVersion: String by settings
         id("fabric-loom").version(loomVersion)
-        val kotlinVersion: String by System.getProperties()
-        kotlin("jvm").version(kotlinVersion)
     }
 }
