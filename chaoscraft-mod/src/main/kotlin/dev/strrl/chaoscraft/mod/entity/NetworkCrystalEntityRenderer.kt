1ace15f5a0b7ead7077b40c75a1e50e7152963c6
lrrtuising <lrrtuising@outlook.com>
2022-01-06 15:00:41 +0800

    add enderDragon.randerCrystalBeam

--- chaoscraft-mod/src/main/kotlin/dev/strrl/chaoscraft/mod/entity/NetworkCrystalEntityRenderer.kt
+++ chaoscraft-mod/src/main/kotlin/dev/strrl/chaoscraft/mod/entity/NetworkCrystalEntityRenderer.kt
@@ -1,10 +1,7 @@
 package dev.strrl.chaoscraft.mod.entity
 
 import net.minecraft.client.model.*
-import net.minecraft.client.render.Frustum
-import net.minecraft.client.render.OverlayTexture
-import net.minecraft.client.render.RenderLayer
-import net.minecraft.client.render.VertexConsumerProvider
+import net.minecraft.client.render.*
 import net.minecraft.client.render.entity.EnderDragonEntityRenderer
 import net.minecraft.client.render.entity.EntityRenderer
 import net.minecraft.client.render.entity.EntityRendererFactory
@@ -12,9 +9,7 @@ import net.minecraft.client.render.entity.model.EntityModelLayers
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.entity.decoration.EndCrystalEntity
 import net.minecraft.util.Identifier
-import net.minecraft.util.math.MathHelper
-import net.minecraft.util.math.Quaternion
-import net.minecraft.util.math.Vec3f
+import net.minecraft.util.math.*
 import kotlin.math.sin
 
 class NetworkCrystalEntityRenderer(
@@ -27,6 +22,8 @@ class NetworkCrystalEntityRenderer(
     companion object {
         val TEXTURE = Identifier("textures/entity/end_crystal/end_crystal.png")
         val END_CRYSTAL: RenderLayer = RenderLayer.getEntityCutoutNoCull(TEXTURE)
+        private val CRYSTAL_BEAM_LAYER: RenderLayer =
+            RenderLayer.getEntitySmoothCutout(EnderDragonEntityRenderer.CRYSTAL_BEAM_TEXTURE)
         val SINE_45_DEGREES = sin(0.7853981633974483).toFloat()
         fun getTexturedModelData(): TexturedModelData? {
             val modelData = ModelData()
@@ -47,6 +44,65 @@ class NetworkCrystalEntityRenderer(
                 ModelTransform.NONE
             )
             return TexturedModelData.of(modelData, 64, 32)
+        }
+
+        fun renderCrystalBeam(
+            dx: Float,
+            dy: Float,
+            dz: Float,
+            tickDelta: Float,
+            age: Int,
+            matrices: MatrixStack,
+            vertexConsumers: VertexConsumerProvider,
+            light: Int
+        ) {
+            val f = MathHelper.sqrt(dx * dx + dz * dz)
+            val g = MathHelper.sqrt(dx * dx + dy * dy + dz * dz)
+            matrices.push()
+            matrices.translate(0.0, 2.0, 0.0)
+            matrices.multiply(
+                Vec3f.POSITIVE_Y.getRadialQuaternion(
+                    (-Math.atan2(
+                        dz.toDouble(),
+                        dx.toDouble()
+                    )).toFloat() - 1.5707964f
+                )
+            )
+            matrices.multiply(
+                Vec3f.POSITIVE_X.getRadialQuaternion(
+                    (-Math.atan2(
+                        f.toDouble(),
+                        dy.toDouble()
+                    )).toFloat() - 1.5707964f
+                )
+            )
+            val vertexConsumer = vertexConsumers.getBuffer(CRYSTAL_BEAM_LAYER)
+            val h = 0.0f - (age.toFloat() + tickDelta) * 0.01f
+            val i = MathHelper.sqrt(dx * dx + dy * dy + dz * dz) / 32.0f - (age.toFloat() + tickDelta) * 0.01f
+            // val j: Int = 0
+            var k = 0.0f
+            var l = 0.75f
+            var m = 0.0f
+            val entry = matrices.peek()
+            val matrix4f = entry.model
+            val matrix3f = entry.normal
+            for (n in 1..8) {
+                val o = MathHelper.sin(n.toFloat() * 6.2831855f / 8.0f) * 0.75f
+                val p = MathHelper.cos(n.toFloat() * 6.2831855f / 8.0f) * 0.75f
+                val q = n.toFloat() / 8.0f
+                vertexConsumer.vertex(matrix4f, k * 0.2f, l * 0.2f, 0.0f).color(0, 0, 0, 255).texture(m, h)
+                    .overlay(OverlayTexture.DEFAULT_UV).light(light).normal(matrix3f, 0.0f, -1.0f, 0.0f).next()
+                vertexConsumer.vertex(matrix4f, k, l, g).color(255, 255, 255, 255).texture(m, i)
+                    .overlay(OverlayTexture.DEFAULT_UV).light(light).normal(matrix3f, 0.0f, -1.0f, 0.0f).next()
+                vertexConsumer.vertex(matrix4f, o, p, g).color(255, 255, 255, 255).texture(q, i)
+                    .overlay(OverlayTexture.DEFAULT_UV).light(light).normal(matrix3f, 0.0f, -1.0f, 0.0f).next()
+                vertexConsumer.vertex(matrix4f, o * 0.2f, p * 0.2f, 0.0f).color(0, 0, 0, 255).texture(q, h)
+                    .overlay(OverlayTexture.DEFAULT_UV).light(light).normal(matrix3f, 0.0f, -1.0f, 0.0f).next()
+                k = o
+                l = p
+                m = q
+            }
+            matrices.pop()
         }
     }
 
@@ -130,7 +186,7 @@ class NetworkCrystalEntityRenderer(
         core!!.render(matrixStack, vertexConsumer, i, k)
         matrixStack.pop()
         matrixStack.pop()
-        val blockPos = networkCrystalEntity.beamTarget
+        val blockPos = BlockPos(networkCrystalEntity.x + 5, networkCrystalEntity.y + 5, networkCrystalEntity.z + 5)
         if (blockPos != null) {
             val m = blockPos.x.toFloat() + 0.5f
             val n = blockPos.y.toFloat() + 0.5f
@@ -139,7 +195,11 @@ class NetworkCrystalEntityRenderer(
             val q = (n.toDouble() - networkCrystalEntity.y).toFloat()
             val r = (o.toDouble() - networkCrystalEntity.z).toFloat()
             matrixStack.translate(p.toDouble(), q.toDouble(), r.toDouble())
-            EnderDragonEntityRenderer.renderCrystalBeam(
+            val lightAbove = WorldRenderer.getLightmapCoordinates(
+                networkCrystalEntity!!.world,
+                BlockPos(networkCrystalEntity.pos).up()
+            )
+            renderCrystalBeam(
                 -p,
                 -q + h,
                 -r,
@@ -147,7 +207,7 @@ class NetworkCrystalEntityRenderer(
                 networkCrystalEntity.endCrystalAge,
                 matrixStack,
                 vertexConsumerProvider,
-                i
+                lightAbove
             )
         }
         super.render(networkCrystalEntity, f, g, matrixStack, vertexConsumerProvider, i)
@@ -159,4 +219,5 @@ class NetworkCrystalEntityRenderer(
         g = (g * g + g) * 0.4f
         return g - 1.4f
     }
+
 }
